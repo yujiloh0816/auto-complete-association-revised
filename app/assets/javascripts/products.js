fb55c7cd5a32bcd91e75b6e6d7d5a8295fcb9498
yujiloh0816 <imp.yfujiwara@gmail.com>
2019-05-27 18:00:42 +0900

    Add autocomplite with js

---
+++ app/assets/javascripts/products.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+  $('#product_category_name').autocomplete({
+  source: ['foo', 'food', 'four']
+  });
+});
+
+// $( function() {
+//   var availableTags = [
+//     "ActionScript",
+//     "AppleScript",
+//     "Asp",
+//     "BASIC",
+//     "C",
+//     "C++",
+//     "Clojure",
+//     "COBOL",
+//     "ColdFusion",
+//     "Erlang",
+//     "Fortran",
+//     "Groovy",
+//     "Haskell",
+//     "Java",
+//     "JavaScript",
+//     "Lisp",
+//     "Perl",
+//     "PHP",
+//     "Python",
+//     "Ruby",
+//     "Scala",
+//     "Scheme"
+//   ];
+//   $( "#tags" ).autocomplete({
+//     source: availableTags
+//   });
+// } );
+// </script>
